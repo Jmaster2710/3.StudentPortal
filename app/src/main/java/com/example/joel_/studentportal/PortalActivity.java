2fe667e6253f7f48062100b5cddb1080551209de
Jmaster2710 <joel_kroon@hotmail.com>
2018-09-23 14:59:04 +0200

    You can now use the webview

--- app/src/main/java/com/example/joel_/studentportal/PortalActivity.java
+++ app/src/main/java/com/example/joel_/studentportal/PortalActivity.java
@@ -2,6 +2,7 @@ package com.example.joel_.studentportal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class PortalActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class PortalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portal);
+
+        String url = "https://www.google.com";
+        WebView myWebView = findViewById(R.id.webview);
+        myWebView.loadUrl(url);
     }
 }
