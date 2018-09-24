b0a343154122d24aa02bffa78ab7fadf338dffda
Jmaster2710 <joel_kroon@hotmail.com>
2018-09-24 13:20:23 +0200

    Now the webview works!

--- app/src/main/java/com/example/joel_/studentportal/PortalActivity.java
+++ app/src/main/java/com/example/joel_/studentportal/PortalActivity.java
@@ -3,6 +3,7 @@ package com.example.joel_.studentportal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +24,9 @@ public class PortalActivity extends AppCompatActivity {
 
         String url = mUrl;
         WebView myWebView = findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         myWebView.loadUrl(url);
     }
 }
